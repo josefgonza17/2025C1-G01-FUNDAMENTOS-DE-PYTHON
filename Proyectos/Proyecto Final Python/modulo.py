ae91d186bfead7b3da460d7a61b88d0ef208dfe7
Jose F Gonzalez Salgado <josefgonza17@gmail.com>
2025-04-27 17:42:26 -0600

    proyecto

--- Proyectos/Proyecto Final Python/modulo.py
+++ Proyectos/Proyecto Final Python/modulo.py
@@ -1,9 +1,26 @@
 
 import pandas as pd
 
-ruta_archivo_csv = "DatosAbiertos_Ingresos_Dic2024.csv" #Se define la ruta del archivo
+#Importar datos para el 2024
 
-df = pd.read_csv(ruta_archivo_csv, delimiter=',')
+ruta_2024 = "/Users/josefgonzalez/Documents/GitHub/2025C1-G01-FUNDAMENTOS-DE-PYTHON/Proyectos/Proyecto Final Python/DatosAbiertos_Ingresos_Dic2024.csv"
 
-df
+df_2024 = pd.read_csv(ruta_2024, delimiter=",", encoding="latin-1")
+
+#Importar datos para el 2023
+
+ruta_2023 = "/Users/josefgonzalez/Documents/GitHub/2025C1-G01-FUNDAMENTOS-DE-PYTHON/Proyectos/Proyecto Final Python/DatosAbiertosDic23_Ingresos.csv"
+
+df_2023 = pd.read_csv(ruta_2024, delimiter=",", encoding="latin-1")  
+
+
+#Importar datos para el 2022  
+
+ruta_2022 = "/Users/josefgonzalez/Documents/GitHub/2025C1-G01-FUNDAMENTOS-DE-PYTHON/Proyectos/Proyecto Final Python/DatosAbiertosIngresos2022-12.csv"
+
+df_2022 = pd.read_csv(ruta_2024, delimiter=",", encoding="latin-1")
+
+print("Datos del 2024: ", df_2024.shape)
+
+#Consolidacion dataframes 3 años
 
